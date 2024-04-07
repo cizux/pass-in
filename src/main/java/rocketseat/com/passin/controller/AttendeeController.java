3f930bbf527d32343cf50f4f5d926dcb3614f0ba
Tarcisio Paz Junger <tarcisiojunger@gmail.com>
2024-04-06 21:06:12 -0300

    Created Badge End Point

--- src/main/java/rocketseat/com/passin/controller/AttendeeController.java
+++ src/main/java/rocketseat/com/passin/controller/AttendeeController.java
@@ -2,16 +2,28 @@ package rocketseat.com.passin.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import lombok.RequiredArgsConstructor;
+import rocketseat.com.passin.dto.attendee.AttendeeBadgeResponseDTO;
+import rocketseat.com.passin.service.AttendeeService;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/attendees")
 public class AttendeeController {
+	
+	private final AttendeeService attendeeService;
 
-	@GetMapping
-	public ResponseEntity<String> getTest(){
-		return ResponseEntity.ok("Sucesso");
+	@GetMapping("/{attendeeId}/badge")
+	public ResponseEntity<AttendeeBadgeResponseDTO> getAttendeeBadge(@PathVariable String attendeeId, UriComponentsBuilder uriComponentsBuilder){
+		AttendeeBadgeResponseDTO response =	this.attendeeService.getAttendeeBadge(attendeeId, uriComponentsBuilder);
+		
+		
+		return ResponseEntity.ok(response);
 	}
 	
 }
